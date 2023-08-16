55206d7cf66a2d6e046c7dbb69b2b9d2cb00b021
Nobuo1106 <gonnbe1986@gmail.com>
2023-08-16 23:14:14 +0900

    interceptionでHTTPヘッダーを送信

--- app/src/main/java/com/websarva/wings/android/qiitareadersample/RetrofitService.kt
+++ app/src/main/java/com/websarva/wings/android/qiitareadersample/RetrofitService.kt
@@ -1,6 +1,8 @@
 package com.websarva.wings.android.qiitareadersample
 
 import com.websarva.wings.android.qiitareadersample.model.Article
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
 import retrofit2.http.GET
 import retrofit2.Call
 import retrofit2.Retrofit
@@ -9,13 +11,25 @@ import retrofit2.converter.gson.GsonConverterFactory
 interface RetrofitService {
     @GET("api/v2/items")
     fun getAllArticles(): Call<List<Article>>
+
     companion object {
         var retrofitService: RetrofitService? = null
         fun getInstance(): RetrofitService {
             if (retrofitService == null) {
+                val httpClient = OkHttpClient.Builder()
+                httpClient.addInterceptor { chain ->
+                    val original = chain.request()
+                    val requestBuilder = original.newBuilder()
+                        .header("Authorization", "Bearer 818187039180032e9d0ca44c2a3056ced1d12fac")
+                        .method(original.method(), original.body())
+                    val request = requestBuilder.build()
+                    chain.proceed(request)
+                }
+
                 val retrofit = Retrofit.Builder()
                     .baseUrl("https://qiita.com/")
                     .addConverterFactory(GsonConverterFactory.create())
+                    .client(httpClient.build())
                     .build()
                 retrofitService = retrofit.create(RetrofitService::class.java)
             }
