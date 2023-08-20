ece5300ddc2f29f7bb3ad1f71b55a8a91ec96f48
Nobuo1106 <gonnbe1986@gmail.com>
2023-08-20 11:19:23 +0900

    ボタンを画像に変更

--- app/src/main/java/com/websarva/wings/android/qiitareadersample/WebViewActivity.kt
+++ app/src/main/java/com/websarva/wings/android/qiitareadersample/WebViewActivity.kt
@@ -3,18 +3,15 @@ package com.websarva.wings.android.qiitareadersample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebView
-import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 
 class WebViewActivity : AppCompatActivity() {
-    private lateinit var webView: WebView
-    private lateinit var closeButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web_view)
 
         val webView: WebView = findViewById(R.id.web_view)
-        closeButton = findViewById<Button>(R.id.close_button) // update this line
+        val closeButton = findViewById<ImageView>(R.id.close_button) // update this line
         val url = intent.getStringExtra("url")
         if (url != null) {
             webView.loadUrl(url)
