f35aac41281eb6480cc140c412c00fab48f43273
Nobuo1106 <gonnbe1986@gmail.com>
2023-08-20 10:12:57 +0900

    閉じるボタン実装

--- app/src/main/java/com/websarva/wings/android/qiitareadersample/WebViewActivity.kt
+++ app/src/main/java/com/websarva/wings/android/qiitareadersample/WebViewActivity.kt
@@ -3,16 +3,25 @@ package com.websarva.wings.android.qiitareadersample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebView
+import android.widget.Button
+import android.widget.TextView
 
 class WebViewActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+    private lateinit var closeButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web_view)
 
         val webView: WebView = findViewById(R.id.web_view)
+        closeButton = findViewById<Button>(R.id.close_button) // update this line
         val url = intent.getStringExtra("url")
         if (url != null) {
             webView.loadUrl(url)
+        }
+
+        closeButton.setOnClickListener {
+            finish()
         }
     }
 }
